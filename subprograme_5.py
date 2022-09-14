0c7748d49743b3725b6ed13d7b2079a6c2027b8d
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2022-09-14 17:14:47 +0300

    Update subprograme_5.py

--- subprograme_5.py
+++ subprograme_5.py
@@ -5,4 +5,8 @@ d=int(input())
 def adunarea(w,x,y,z):
     s=(w*z+y*x)/(x*z)
     return s
-print(s(a,b,c,d))
+print('adunarea:',adunarea(a,b,c,d))
+def inmultirea(w,x,y,z):
+    i=(y*w)/(x*z)
+    return i
+print('inmultirea:',inmultirea(a,b,c,d))
